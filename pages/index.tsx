5d8aca28c820a71424b2cc7b4fe12a15b7d240c8
GasparNicoulaud <g.nicoulaud@gmail.com>
2022-11-08 15:35:37 +0100

    ça marche ?

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import {Button} from "../lib/ui/Button";
 export default function Home() {
   return (
     <>
-    <h1>Wesh</h1>
+    <h1>Wesh ça marche</h1>
     <Button title="Clique" onClick={()=>console.log('cliqué')}/>
     </>
   )
