37bad29e15ddbc7b262de3d48d1145d171bd8f4a
GasparNicoulaud <g.nicoulaud@gmail.com>
2022-11-08 18:05:05 +0100

    ça marche

--- pages/sondage/[id].tsx
+++ pages/sondage/[id].tsx
@@ -1,11 +1,13 @@
-import { getDocs } from 'firebase/firestore'
+import { getDocs, query, where } from 'firebase/firestore'
 import { GetServerSidePropsContext } from 'next'
 import { useRouter } from 'next/router'
 import { app } from '../../lib/db'
 
-export async function getServerSideProps({req,res}:GetServerSidePropsContext) {
+export async function getServerSideProps(context:GetServerSidePropsContext) {
+
+  console.log(context.params?.id)
   
-  const sondage = await getDocs(app)
+  const sondage = await getDocs(query(app, where("SondageId", "==", context.params?.id)))
   const data = sondage.docs.map(el => el.data())
   console.log(data)
 
