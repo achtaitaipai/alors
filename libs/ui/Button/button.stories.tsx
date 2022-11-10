94c9be8dce9e3f5038241be56a2d8f72a5a00c77
achtaitaipai <charles.cailleteau@gmail.com>
2022-11-10 14:50:01 +0100

    snippets for tsx and ladle

--- libs/ui/Button/button.stories.tsx
+++
@@ -1,4 +0,0 @@
-import {Button} from ".";
-
-export const story = () => <Button title="Clique"/>;
-story.storyName="Button"
--- libs/ui/Button/style.css.ts
+++
@@ -1,6 +0,0 @@
-import { style } from "@vanilla-extract/css";
-
-export const buttonStyle = style({
-    color:'indigo',
-    margin:5,
-})
