5a158742846c1fb7c3e7c67591501df7978c5dae
GasparNicoulaud <g.nicoulaud@gmail.com>
2022-11-18 01:21:08 +0100

    footer et logo

--- libs/ui/Footer/style.css.ts
+++ libs/ui/Footer/style.css.ts
@@ -1,30 +1,32 @@
 import { style } from '@vanilla-extract/css'
+import { url } from 'inspector'
 import { colors } from '../../style/colors/index.css'
 import { vars } from '../../style/variables/index.css'
 
 export const footerStyle = style({
 	width: '100vw',
-	height: '30px',
 	padding: '5px',
 	background: colors.brand4,
 	color: colors.neutral12,
-	fontSize: '1rem'
+	fontSize: '1rem',
+	display:'flex',
+	alignItems:'center',
+	marginTop:'auto'
 })
 
 export const footerLogoStyle = style({
-	width: '100vw',
-	height: '30px',
+	width: '60px',
+	height: '60px',
 	padding: '5px',
-	background: colors.brand4,
-	color: colors.neutral12,
-	fontSize: '1rem'
+	backgroundImage: "url('/alorsV1ps.png')",
+	backgroundSize: 'contain',
+	display: 'block',
+	marginRight:'auto'
 })
 
 export const footerLinkStyle = style({
-	width: '100vw',
-	height: '30px',
-	padding: '5px',
+	paddingRight: '20px',
 	background: colors.brand4,
-	color: colors.neutral12,
+	color: colors.neutral8,
 	fontSize: '1rem'
 })
--- styles/layouts/container.css.ts
+++ styles/layouts/container.css.ts
@@ -2,7 +2,8 @@ import { style } from '@vanilla-extract/css'
 import { colors } from '../../libs/style/colors/index.css'
 
 export const containerStyle = style({
-	width: '100%',
-	height: '100%',
+	display: 'flex',
+	flexDirection: 'column',
+	minHeight: "100%",
 	backgroundColor: colors.neutral2,
 })
